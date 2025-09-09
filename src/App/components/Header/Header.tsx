4a5bb7672f60e703919c20b772d35f9b33d3307b
Eugena <eugena.h12345@gmail.com>
2025-09-09 19:52:45 +0300

    add header and headerNav

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -1,9 +1,17 @@
+import HeaderNav from '../HeaderNav/HeaderNav';
 import styles from './Header.module.scss';
+
 
 const Header = () => {
     return (
         <div className={styles.headerContainer}>
-            Шапка
+            <div>Logo</div>
+            <div>Food Client</div>
+            <HeaderNav/>
+            <div>
+                <div>сердечко</div>
+                <div>человечек</div>
+            </div>
         </div>
     )
 };
---
+++ src/App/components/HeaderNav/HeaderNav.tsx
@@ -0,0 +1,55 @@
+//import { Link } from 'react-router';
+import styles from './HeaderNav.module.scss';
+//import { routes } from 'config/routes.config';
+import { useState } from 'react';
+
+const menuItems = [
+    { label: 'Recipes', },//route: routes.recipes.create()
+    { label: 'Meals Categories', },
+    { label: 'Products', },
+    { label: 'Menu Items', },
+    { label: 'Meal Planning', },
+];
+
+const HeaderNav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const showMenu = () => setIsMenuOpen(true);
+    const closeMenu = () => setIsMenuOpen(false);
+    return (
+        <>
+            <div className={styles.burger} >
+                <button onClick={showMenu}>☰</button>
+                {isMenuOpen &&
+                    <div className={styles.mobileMenu} onClick={closeMenu}>
+                        {/* <ul>
+                            <li><Link to={routes.main.create()} onClick={closeMenu} >Products</Link></li>
+                            <li><Link to={routes.categories.create()} onClick={closeMenu}>Categories</Link></li>
+                            <li><Link to={routes.about.create()} onClick={closeMenu}>About us</Link></li>
+                        </ul> */}
+                        <button onClick={closeMenu} className={styles.close}>
+                            ×
+                        </button>
+                    </div>
+                }
+
+            </div>
+            <nav className={styles.headerNav}>
+                <ul>
+                    {menuItems.map((item) => {
+                        return (<li>
+                            {/* <Link to={item.route}> */}
+                            {item.label}
+                            {/* </Link> */}
+                        </li>)
+                    })}
+                    {/* <li><Link to={routes.main.create()}>Products</Link></li>
+                    <li><Link to={routes.categories.create()}>Categories</Link></li>
+                    <li><Link to={routes.about.create()}>About us</Link></li> */}
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default HeaderNav;

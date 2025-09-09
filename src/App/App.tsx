0ccfc4c5b0d30fbb71e64bc05105e91c1b2a4c03
Eugena <eugena.h12345@gmail.com>
2025-09-09 19:27:37 +0300

    move App to folder, add sass

---
+++ src/App/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Header/>
+      <div>
+       Основная страница
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/App/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <div className={styles.headerContainer}>
+            Шапка
+        </div>
+    )
+};
+
+export default Header;

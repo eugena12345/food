8baa43f5410b0245468ca8550a521eda20b0eae8
Eugena <eugena.h12345@gmail.com>
2025-09-10 21:27:04 +0300

    add getting recipes with axios

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly REACT_APP_API_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/App/pages/CatalogPage/CatalogPage.tsx
+++ src/App/pages/CatalogPage/CatalogPage.tsx
@@ -1,16 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+const STRAPI_BASE_URL = 'https://front-school-strapi.ktsdev.ru';
+const STRAPI_URL = `${STRAPI_BASE_URL}/api`;
+
+
+
+
 const CatalogPage = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            const response = await axios.get(
+                `${STRAPI_URL}/recipes`,
+                {
+                    headers: {
+                        // API_TOKEN нужно получить в боте при выборе проекта
+                        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+                    },
+                },
+            );
+
+            console.log('response', response);
+            setRecipes(response.data.data);
+        };
+
+        fetch();
+    }, []);
+
     return (
         <div>Основная страница
 
-            {/* TODO вывод карточек товаров после загрузки с сервера <ul>
-                {USERS.map(user => (
-                    <li key={user.id}>
-                        <Link to={`/users/${user.id}`}>
-                            {user.name}
+            <ul>
+                {recipes.map(rec => (
+                    <li key={rec.id}>
+                        <Link to={`/users/${rec.id}`}>
+                            {rec.name}
                         </Link>
                     </li>
                 ))}
-            </ul> */}
+            </ul>
         </div>
     )
 };

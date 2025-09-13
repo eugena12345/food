8d3849f469e65a4d2cd6970750e6fba0a40ac97f
Eugena <eugena.h12345@gmail.com>
2025-09-14 00:29:03 +0300

    update header styles

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -3,27 +3,37 @@ import styles from './Header.module.scss';
 import logo from 'assets/images/Group.svg';
 import userSvg from 'assets/images/User.svg'
 import heartSvg from 'assets/images/HeartIcon.svg'
+import { useNavigate } from 'react-router';
+import { routes } from 'config/routes.config';
 
 
 const Header = () => {
+    const navigate = useNavigate()
     const goToLogin = () => {
-        //TODO переадресация на страницу авторизации
+        navigate(routes.login.create())
     }
 
     const goToFavorite = () => {
-        //TODO переадресация на страницу любимых блюд
+        navigate(routes.favorite.create())
+    }
+
+    const goToCatalog = () => {
+        navigate(routes.main.create())
+
     }
 
     return (
-        <div className={styles.headerContainer}>
-            <div className={styles.logoContent}>
-                <img src={logo} alt='logo' className={styles.logo} />
-                <div>Food Client</div>
-            </div>
-            <HeaderNav />
-            <div>
-                <img src={heartSvg} alt='heartSvg' className={styles.userInfo} onClick={goToFavorite} />
-                <img src={userSvg} alt='userSvg' className={styles.userInfo} onClick={goToLogin} />
+        <div className={styles.generalHeaderContainer}>
+            <div className={styles.headerContainer}>
+                <div className={styles.logoContent} onClick={goToCatalog}>
+                    <img src={logo} alt='logo' className={styles.logo} />
+                    <div className={styles.logoTitle}>Food Client</div>
+                </div>
+                <HeaderNav />
+                <div className={styles.logoContent}>
+                    <img src={heartSvg} alt='heartSvg' className={styles.userInfo} onClick={goToFavorite} />
+                    <img src={userSvg} alt='userSvg' className={styles.userInfo} onClick={goToLogin} />
+                </div>
             </div>
         </div>
     )

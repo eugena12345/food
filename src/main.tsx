fd9778ff734d19ef350cdbb4fd5a68a1549a2520
eugena <eugena.h12345@gmail.com>
2025-09-18 17:02:20 +0300

    configure mob-x

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { routesConfig } from '~config/routes';
+import '~config/configureMobX';
 
 const router = createBrowserRouter(routesConfig)
 

516951b36d81a4a3e14ad9e4ee017519a415e443
eugena <eugena.h12345@gmail.com>
2025-09-24 16:15:44 +0300

    remove unnecessary import

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import Header from './components/Header';
-import CatalogPage from './pages/CatalogPage';
 import { useQueryParamsStoreInit } from '~store/RootStore/hooks/useQueryParamsStoreInit';
 
 function App() {
